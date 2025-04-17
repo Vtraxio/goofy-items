0490c80a7f37fc4de81976b5db712bd7e2fbf3d4
Vtraxio <55654178+Vtraxio@users.noreply.github.com>
2025-04-17 15:46:06 +0200

    Linting setup

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
+export default tseslint.config(
+  {
+    ignores: ["**/dist/*", "eslint.config.mjs", "tsconfig.json"],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+);
